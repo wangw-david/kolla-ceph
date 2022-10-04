323b4977c892b0a285796ad9afb9d85f398c9550
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 03:24:35 +0000

    Adding numeric_owner as keyword arguement

--- kolla/image/build.py
+++ kolla/image/build.py
@@ -396,7 +396,7 @@ class BuildTask(DockerTask):
                                 if not is_within_directory(path, member_path):
                                     raise Exception("Attempted Path Traversal in Tar File")
                         
-                            tar.extractall(path, members, numeric_owner) 
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
                             
                         
                         safe_extract(archive_tar, path=items_path)
